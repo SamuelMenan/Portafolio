ce6697a6f70ab7fee45ad6ea5ebd40850c314b42
SamuelMenan <sam100uel@gmail.com>
2026-04-09 11:55:50 -0500

    mejora de tipografia

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL@0..1"
