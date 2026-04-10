cf3ee2881ab1565562950f8e0ced9a1857eebe06
SamuelMenan <sam100uel@gmail.com>
2026-04-10 08:22:03 -0500

    correcion de node issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="darkreader-lock" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
